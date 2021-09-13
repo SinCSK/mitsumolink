09cfdcfbf3acdae6dd627ac74590123cd5815766
SinCSK <otomeza.man01@gmail.com>
2021-09-13 17:37:41 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'quotations#index'
+  resources :quotations, only: [:index, :new, :create, :show, :edit, :destroy]
+  resources :items, only: [:new, :create, :destroy]
 end
